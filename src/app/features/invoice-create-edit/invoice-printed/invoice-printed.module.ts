31c08c73a4700c1429a9a2fe48d3ced93a2280cf
scepan_jankovic <scepan.jankovic@material-exchange.com>
2023-06-28 13:32:48 +0200

    Return of QR code to project

--- src/app/features/invoice-create-edit/invoice-printed/invoice-printed.module.ts
+++ src/app/features/invoice-create-edit/invoice-printed/invoice-printed.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { InvoicePrintedComponent } from './invoice-printed.component';
 import { FramingDescriptionModule } from 'src/app/shared/components/framing-description/framing-description.module';
 import { TranslateModule } from '@ngx-translate/core';
-// import { QRCodeModule } from 'angularx-qrcode';
+import { QRCodeModule } from 'angularx-qrcode';
 import { CustomNumberPipeModule } from 'src/app/core/pipes/custom-number-pipe.module';
 
 @NgModule({
@@ -12,7 +12,7 @@ import { CustomNumberPipeModule } from 'src/app/core/pipes/custom-number-pipe.mo
     CommonModule,
     FramingDescriptionModule,
     TranslateModule,
-    // QRCodeModule,
+    QRCodeModule,
     CustomNumberPipeModule,
   ],
   exports: [InvoicePrintedComponent],
